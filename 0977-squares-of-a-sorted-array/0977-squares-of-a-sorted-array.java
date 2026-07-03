769f15437cd889955ff3e2adc2e8b14a2f0d5a69
Arnav Shukla <140689556+Arnavshukla09@users.noreply.github.com>
2026-07-04 00:21:18 +0530

    Time: 1 ms (100%), Space: 47.3 MB (53.14%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -4,40 +4,24 @@ class Solution {
         int n = nums.length;
         int[] result = new int[n];
 
-        // Find the first non-negative number
-        int right = 0;
-        while (right < n && nums[right] < 0) {
-            right++;
-        }
+        int left = 0;
+        int right = n - 1;
+        int k = n - 1;
 
-        int left = right - 1;
-        int k = 0;
-
-        // Merge the two halves
-        while (left >= 0 && right < n) {
+        while (left <= right) {
 
             int leftSquare = nums[left] * nums[left];
             int rightSquare = nums[right] * nums[right];
 
-            if (leftSquare <= rightSquare) {
-                result[k++] = leftSquare;
-                left--;
+            if (leftSquare > rightSquare) {
+                result[k] = leftSquare;
+                left++;
             } else {
-                result[k++] = rightSquare;
-                right++;
+                result[k] = rightSquare;
+                right--;
             }
-        }
 
-        // Copy remaining left side
-        while (left >= 0) {
-            result[k++] = nums[left] * nums[left];
-            left--;
-        }
-
-        // Copy remaining right side
-        while (right < n) {
-            result[k++] = nums[right] * nums[right];
-            right++;
+            k--;
         }
 
         return result;
